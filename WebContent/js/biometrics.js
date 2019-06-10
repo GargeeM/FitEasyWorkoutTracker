9cf6eb3f57066ef83910bdd1862769aa566a32ff
MohitSrivastava <MohitSrivastava@DESKTOP-MOTPDAP.in.ibm.com>
2019-06-10 17:19:15 +0530

    Put the message of ideal body weight in javascript,corrected background image , took age as input . [Mohit]

--- WebContent/js/biometrics.js
+++ WebContent/js/biometrics.js
@@ -8,3 +8,60 @@ function workPlanFunc()
 	}
 	return true;
 }
+
+
+//Using button
+
+function idealWeight()
+{
+
+	// $(document).ready(function(){
+	// 	  $("#resultButton").click(function(){
+	// 	    $("#resultantText").hide();
+	// 	  });
+	// 	});
+
+	if(document.getElementById('male').checked == false && document.getElementById('female').checked == false){
+		alert("Please select gender !!!")
+	}
+	else if(document.getElementById('male').checked) {
+		
+		var ht = document.getElementById('height').value;
+	  	var ibw = Math.ceil(50.0 + 2.3 * ((ht / 2.54) - 60.0));  /*Body mass formula for male*/
+		weight = document.getElementById('weight').value;
+		var result = result(ibw , weight);
+		document.getElementById("resultantText").innerHTML = result; 
+		document.getElementById("resultantText").style.color = "white";
+	}
+	else if(document.getElementById('female').checked){
+		var ht = document.getElementById('height').value;
+		var ibw = Math.ceil(45.5 + 2.3 * ((ht/ 2.54) - 60.0));  /*Body mass formula for female*/
+		weight = document.getElementById('weight').value;
+		var result = result(ibw , weight);
+		document.getElementById("resultantText").innerHTML = result; 
+		document.getElementById("resultantText").style.color = "white"; 
+	}
+
+
+function result(ibw , weight)
+{
+	if(weight > ibw)
+	{
+		var res = "You need to lose "+(weight - ibw)+" kgs to reach ideal weight";
+		return res;
+	}
+	else if(weight < ibw)
+	{
+		var res = "You need to gain "+(ibw - weight)+" kgs to reach ideal weight";
+		return res;
+	}
+	else if(weight = ibw)
+	{
+		var res = "You have reached your ideal body weight ! Workout regularly to maintain it !";
+		return res;
+	}
+}
+}
+
+
+
